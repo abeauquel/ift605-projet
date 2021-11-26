6ecd16861b1570a095c551220bbeed9f767ab5de
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-11-26 13:12:28 -0500

    bluetooth wiht websocket

--- pi/app/main.js
+++ pi/app/main.js
@@ -4,25 +4,6 @@ const heartbeat = require('./src/heartbeat.js');
 const exercice = require('./src/exercice.js');
 const actionFunctions = require('./src/action');
 
-listenerRFCOMM = function(val) {
-    let messages = actionFunctions.parseMessages(val);
-    for (let message of messages) {
-        console.log(message);
-        let action = actionFunctions.deserializeAction(message);
-
-        console.log(action);
-    }
-}
-
-listenerConnection = function(val) {
-    if (val)
-        console.log('connection '+ new Date())
-    else
-        console.log('disconnection '+ new Date())
-};
-
-//bluetooth.startALL(listenerRFCOMM, listenerConnection);
-
 
 //Server Web
 const hostname = "127.0.0.1";
@@ -62,6 +43,26 @@ wss.on('connection', ws => {
 
 
 let start = async function(ws){
+
+    let listenerRFCOMM = function (val) {
+        let messages = actionFunctions.parseMessages(val);
+        for (let message of messages) {
+            console.log(message);
+            let action = actionFunctions.deserializeAction(message);
+
+            console.log(action);
+        }
+    }
+
+    let listenerConnection = function (val) {
+        if (val)
+            console.log('connection ' + new Date()); // todo envoyer : est connecté
+        else
+            console.log('disconnection ' + new Date())
+    };
+
+    //bluetooth.startALL(listenerRFCOMM, listenerConnection);
+
     heartbeat.start(ws);
     exercice.start(ws);
     sensor.start(ws);
