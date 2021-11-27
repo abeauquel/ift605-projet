223fc1f72d9892ca8da3f3963ea4bcded5b7c3f6
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-11-27 11:22:11 -0500

    wip kettle swing

--- pi/app/src/exercice.js
+++ pi/app/src/exercice.js
@@ -4,39 +4,45 @@ const ACTION = 'exercice';
 let start = async function (ws){
     console.log("exercice.js"+ new Date());
 
-    let name = "Squat";
+    let name = "Kettlebell swing";
     let repetitionInSet = 12;
     let repetition = 12;
     let set = 5;
     let tic = 0;
-    let variationY = -0.05;
+    let sens = 1;
     let doubleTic =0;
+    let x =0;
+    let y= 0;
+    let z = 0;
+    let variation = 0.08;
     let interval = setInterval(function(str1, str2) {
-            let data = {};
-            data.action = ACTION;
-            data.name= name;
-            data.repetition = repetition;
-            data.set = set;
-            data.y = variationY;
-            data.z = 0.0;
-            data.x = 0.0;
-            ws.send(JSON.stringify(data));
+        y += variation * sens;
+        x += variation * sens;
+        let data = {};
+        data.action = ACTION;
+        data.name= name;
+        data.repetition = repetition;
+        data.set = set;
+        data.y = y;
+        data.x = x;
+        data.z = 0.0;
+        ws.send(JSON.stringify(data));
 
-            tic+=1;
-            if(tic > 40){
-                tic =0;
-                doubleTic+=1;
-                variationY= variationY * -1;
-                if(doubleTic === 2){
-                    doubleTic = 0;
-                    repetition--;
-                    if(repetition < 1){
-                        set--;
-                        repetition = repetitionInSet;
-                    }
+        tic+=1;
+        if(tic > 45){
+            tic =0;
+            doubleTic+=1;
+            sens = sens * -1;
+            if(doubleTic === 2){
+                doubleTic = 0;
+                repetition--;
+                if(repetition < 1){
+                    set--;
+                    repetition = repetitionInSet;
                 }
-
             }
+
+        }
 
 
     }, tStep, "Hello.", "How are you?");
